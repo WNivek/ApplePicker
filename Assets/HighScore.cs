3114b8996316d0d9d967ec4e2a2edf6ffc2ed245
Kevin W <wnivek@gmail.com>
2016-09-22 21:56:07 -0400

    High Score tracking

---
+++ Assets/HighScore.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+    static public int score = 1000;
+	
+	void Update () {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+	}
+}
