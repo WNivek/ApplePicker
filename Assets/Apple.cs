09d5d3f6ccb4d6b93c46b38351ca97a6cb5a9cf9
Kevin W <wnivek@gmail.com>
2016-09-22 20:38:59 -0400

    Dropping apples are now destroyed.

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class Apple : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    public static float bottomY = -20f;
+
 	void Update () {
-	
+	    if (transform.position.y < bottomY) {
+            Destroy(this.gameObject);
+        }
 	}
 }
