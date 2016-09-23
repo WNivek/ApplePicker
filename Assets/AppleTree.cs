825b1c965bfa0da8569701427c826731bad01003
Kevin W <wnivek@gmail.com>
2016-09-22 20:01:10 -0400

    Setting up AppleTree variables

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class AppleTree : MonoBehaviour {
 
-	// Use this for initialization
+
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves in meters/second
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+
 	void Start () {
-	
+	    // Dropping apples every second
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+        // Basic Movement
+	    // Changing Direction
 	}
 }
